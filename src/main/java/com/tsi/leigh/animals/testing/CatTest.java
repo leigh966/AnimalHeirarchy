fea92f60c6c29d697027328139b1f90a65082837
leigh966 <leigh966@hotmail.com>
2022-01-24 16:06:24 +0000

    Removed unused dependencies

--- src/main/java/com/tsi/leigh/animals/testing/CatTest.java
+++ src/main/java/com/tsi/leigh/animals/testing/CatTest.java
@@ -2,7 +2,6 @@ package com.tsi.leigh.animals.testing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import com.tsi.leigh.animals.Animal;
 import com.tsi.leigh.animals.Cat;
 
 public class CatTest
