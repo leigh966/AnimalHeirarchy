bc8d52879d0003338bc701787db74c43c3d7c156
leigh966 <leigh966@hotmail.com>
2022-01-24 17:17:58 +0000

    Unit tests should run automatically after moving test class to correct place

---
+++ src/test/java/com/tsi/leigh/animals/TestCat.java
@@ -0,0 +1,16 @@
+package com.tsi.leigh.animals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.tsi.leigh.animals.Cat;
+
+public class TestCat
+{
+    @Test
+    public void testCat()
+    {
+        Cat c = new Cat();
+        Assertions.assertEquals(4, c.getNumberOfLegs(), "Cat should be created with 4 legs");
+
+    }
+}
