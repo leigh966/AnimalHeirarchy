150a674b1e2933ddff2df0d657284b224459c39a
leigh966 <leigh966@hotmail.com>
2022-01-25 12:29:33 +0000

    Removed useless main code and tests

--- src/main/java/com/tsi/leigh/animals/Main.java
+++ src/main/java/com/tsi/leigh/animals/Main.java
@@ -3,19 +3,8 @@ package com.tsi.leigh.animals;
 public class Main
 {
 
-    public static int main(String[] args)
+    public static void main(String[] args)
     {
-
-            Cat c = new Cat();
-            System.out.println(c.reproduce());
-            c.walk();
-            System.out.println(c.getName() + " has " + c.getNumberOfLegs() + " legs");
-            if(c.isWarmBlooded())
-            {
-                System.out.println(c.getName() + " is warm blooded");
-            }
-
-            return 0;
-
+        System.out.println("Hello World");
     }
 }
